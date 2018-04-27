c7ae9411b8ee328bf9cd3b9a8863de98e2a8beaf
touhouota <touhouota.register@gmail.com>
2018-04-27 15:38:28 +0900

    ボタンをクリック時に、モーダルの初期化をする部分を追記。

--- client/lib/buttons_events.js
+++ client/lib/buttons_events.js
@@ -1,7 +1,13 @@
+import ModalProcess from './modal';
+
 const Buttons = {
   github: () => {
     const URL = 'https://github.com/touhouota/task_scheduler/issues';
     window.open(URL, '_blank');
+  },
+
+  append_task: () => {
+    ModalProcess.init();
   },
 };
 
