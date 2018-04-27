76407a9e638ce5b8f184b55b7e0aeed22689560f
touhouota <touhouota.register@gmail.com>
2018-04-27 15:30:07 +0900

    タスク追加時、モーダルを表示する。 そのためのモーダル制御用のファイルを追加。

---
+++ client/lib/modal.js
@@ -0,0 +1,10 @@
+const ModalProcess = {
+  init: () => {
+    console.log('hoge');
+    const background = document.createElement('div');
+    background.classList.add('bodal_back');
+    document.body.appendChild(background);
+  },
+};
+
+export default ModalProcess;
