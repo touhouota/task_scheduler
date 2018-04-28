c3504dca0209fa981be078e423a6eb99c74837df
touhouota <touhouota.register@gmail.com>
2018-04-28 13:17:10 +0900

    DBにタスクのラベルを追加。

--- db/schema.rb
+++ db/schema.rb
@@ -10,47 +10,46 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_16_151931) do
-
-  create_table "schedules", force: :cascade do |t|
-    t.integer "task_id"
-    t.string "user_id"
-    t.date "start_date"
-    t.date "deadline"
-    t.date "finish_date"
-    t.integer "deleted"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+ActiveRecord::Schema.define(version: 20_180_416_151_931) do
+  create_table 'schedules', force: :cascade do |t|
+    t.integer 'task_id'
+    t.string 'user_id'
+    t.date 'start_date'
+    t.date 'deadline'
+    t.date 'finish_date'
+    t.integer 'deleted'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "tasks", force: :cascade do |t|
-    t.string "user_id"
-    t.string "name"
-    t.integer "status"
-    t.integer "expect_minute"
-    t.integer "actual_sec"
-    t.text "memo"
-    t.text "reflection"
-    t.integer "deleted"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'tasks', force: :cascade do |t|
+    t.string 'user_id'
+    t.string 'name'
+    t.integer 'status'
+    t.string 'label'
+    t.integer 'expect_minute'
+    t.integer 'actual_sec'
+    t.text 'memo'
+    t.text 'reflection'
+    t.integer 'deleted'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "timelines", force: :cascade do |t|
-    t.text "content"
-    t.integer "task_id"
-    t.text "user_id"
-    t.integer "auto", limit: 1
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'timelines', force: :cascade do |t|
+    t.text 'content'
+    t.integer 'task_id'
+    t.text 'user_id'
+    t.integer 'auto', limit: 1
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "user_id"
-    t.string "name"
-    t.integer "group"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'user_id'
+    t.string 'name'
+    t.integer 'group'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
