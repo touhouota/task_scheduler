1058299a9a970d7ce20637211675994c1df556a6
touhouota <touhouota.register@gmail.com>
2018-04-28 11:00:24 +0900

    modalの位置を変更したことによる変更。

--- client/lib/modal_process.jsx
+++ client/lib/modal_process.jsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import Modal from '../components/Modal';
-
 const ModalProcess = {
   init: () => {
     const background = document.createElement('div');
     background.addEventListener('click', ModalProcess.close);
     background.classList.add('modal_back');
     document.body.appendChild(background);
-    ReactDOM.render(<Modal />, background);
+    // modalを表示
+    document.getElementById('modal_area').classList.remove('hide');
   },
 
   close: () => {
     const modalBack = document.querySelector('.modal_back');
     document.body.removeChild(modalBack);
+    // modalを非表示
+    document.getElementById('modal_area').classList.add('hide');
   },
 
   send: () => {
     console.log('send form information');
+    ModalProcess.close();
   },
 };
 
