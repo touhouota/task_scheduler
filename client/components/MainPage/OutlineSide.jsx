1abfb3d72f20d7f6737634c0c5faf6c395f29af7
touhouota <touhouota.register@gmail.com>
2018-05-08 00:18:28 +0900

    アウトラインの構造をなす要素コンポーネントを作り、表示してみた。

--- client/components/MainPage/OutlineSide.jsx
+++ client/components/MainPage/OutlineSide.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 
+import Structure from './Structure';
+
 class OutlineSide extends React.Component {
   render() {
     return (
       <div className="OutlineSide">
-        outline
+        <p>研究の進捗</p>
+        <Structure structureName="テーマ探し" />
+        <Structure structureName="関連研究" />
       </div>
     );
   }
