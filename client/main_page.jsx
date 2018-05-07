193369d82aaaf59e8414b6cc83ca8070c76cbb60
touhouota <touhouota.register@gmail.com>
2018-05-07 22:37:20 +0900

    コンテナをタスク部分だけでなく画面全部にしたので、描画する場所を変更。

--- client/main_page.jsx
+++ client/main_page.jsx
@@ -3,21 +3,15 @@ import ReactDOM from 'react-dom';
 
 // TaskSideコンポーネント
 import MainPage from './components/MainPage';
-
-// 計画に関する処理まとめ
-import Plan from './lib/main_page_plan';
 // ボタンのイベント処理まとめ
 import Buttons from './lib/buttons_events';
 
 window.onload = () => {
-  // 現在時刻に線を引く
-  Plan.currentTimeLine();
-
   // buttonsにイベントを定義//
   document.getElementById('github').addEventListener('click', Buttons.github);
   // タスク追加ボタンのイベントを定義
   document.getElementById('append_task').addEventListener('click', Buttons.append_task);
 
   // タスク一覧部分を描画
-  ReactDOM.render(<MainPage />, document.querySelector('.task_side'));
+  ReactDOM.render(<MainPage />, document.querySelector('.main_container'));
 };
