00d3814f9af4f0e4e477f0225ffd4c63f8f53c04
touhouota <touhouota.register@gmail.com>
2018-05-13 23:28:57 +0900

    念のため、rails側でもvalidationをかける。

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,9 @@
 class Task < ApplicationRecord
   # foreign_key: 紐付け先Tableのカラム名, primary_key: 自分のTableにある結合用のカラム名
   has_one :user, foreign_key: 'user_id', primary_key: 'user_id', class_name: 'User'
+
+  # 入ってくるデータのバリデーション
+  validates :t_name, presence: true
+  validates :expect_minute, presence: true
+  validates :label, presence: true
 end
