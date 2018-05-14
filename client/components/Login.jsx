82a255bcc6911829e50e4bcce15a3c3f870912d3
touhouota <touhouota.register@gmail.com>
2018-05-15 03:20:55 +0900

    functionからclassに変更。 そして、フォームの情報を送るメソッドを追加。

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,15 +1,49 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-const Login = () => (
-  <form>
-    <label>
-        ユーザID:
-      <input type="text" />
-        @fun.ac.jp
-    </label>
-    <button>
-      ログイン
-    </button>
-  </form>
-);
+import Base from '../lib/base_object';
+
+class Login extends React.Component {
+  constructor() {
+    super();
+    this.sendUserInfo = this.sendUserInfo.bind(this);
+  }
+
+  sendUserInfo(event) {
+    const loginForm = document.getElementById('loginForm');
+    const userInfo = new FormData(loginForm);
+    fetch('/api/login/', {
+      method: 'POST',
+      credentials: 'same-origin',
+      headers: {
+        Accept: 'application/json',
+        'X-CSRF-Token': Base.get_token(),
+      },
+      body: userInfo,
+    })
+      .then(response => response.json())
+      .then((json) => {
+        console.log(json);
+      });
+  }
+
+  render() {
+    return (
+      <form id="loginForm">
+        <label>
+            ユーザID:
+          <input type="text" />
+            @fun.ac.jp
+        </label>
+        <br />
+        <button
+          type="button"
+          onClick={(event) => { this.sendUserInfo(event); }}
+        >
+          ログイン
+        </button>
+      </form>
+    );
+  }
+}
+
+export default Login;
