0638e94fc22f7be0a58ba5b9f62ebc7b3e34641d
touhouota <touhouota.register@gmail.com>
2018-05-15 00:59:25 +0900

    ApplicationControllerにTLを入れるためのメソッドを追加。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
-  after_action :check_user
+  # TLを追加する
+  def insert_timeline(json)
+    puts "insert_timeline: #{json}, #{params}"
+    # user_idがない場合は無視する
+    return nil unless params[:user_id]
 
-  def check_user
-    puts "after_action: #{params}"
+    timeline = Timeline.new(json)
+    timeline.save
+    puts "insert_timeline: #{json}"
   end
 end
