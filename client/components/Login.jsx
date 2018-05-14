d3b8587a2f788366d2f0027e11703d9be576fc5a
touhouota <touhouota.register@gmail.com>
2018-05-15 03:04:19 +0900

    loginのためのReactコンポーネントを定義。

---
+++ client/components/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Login = () => (
+  <form>
+    <label>
+        ユーザID:
+      <input type="text" />
+        @fun.ac.jp
+    </label>
+    <button>
+      ログイン
+    </button>
+  </form>
+);
