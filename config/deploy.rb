321ecac216a9c01987380866aeda068756a5b27c
touhouota <touhouota.register@gmail.com>
2018-05-17 20:36:45 +0900

    bindの仕方をunixソケットに変更。

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ append :linked_files, 'config/database.yml', 'config/master.key'
 
 set :puma_threads, [4, 16]
 set :puma_workers, 0
-# set :puma_bind, "#{shared_path}/tmp/sockets/puma.sock"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{shared_path}/log/puma_access.log"
