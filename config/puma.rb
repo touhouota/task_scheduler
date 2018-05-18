d4438845df258315d617ae3342a28727db50ef63
touhouota <touhouota.register@gmail.com>
2018-05-18 21:51:43 +0900

    pumaのportを3000に固定。

--- config/puma.rb
+++ config/puma.rb
@@ -9,7 +9,9 @@ threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port        ENV.fetch('PORT') { 3000 }
+# port        ENV.fetch('PORT') { 3000 }
+bind 'unix://' + File.join(Dir.pwd, 'tmp', 'sockets', 'puma.sock')
+port 3000
 
 # Specifies the `environment` that Puma will run in.
 #
