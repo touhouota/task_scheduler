a969b5d160e2241f5cfd77c33a94875a797c0cc7
touhouota <touhouota.register@gmail.com>
2018-05-21 19:05:07 +0900

    3000版を見るように変更。

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ set :puma_threads, [4, 16]
 set :puma_workers, 0
 # pumaが口を明けていてほしいところを指定する
 # set :puma_bind, "#{shared_path}/tmp/sockets/puma.sock"
-# set :puma_bind, 'http://localhost:3000'
+set :puma_bind, 'tcp://localhost:3000'
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{shared_path}/log/puma_access.log"
