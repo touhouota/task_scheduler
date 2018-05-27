f907cb9a6aadef087407d5463fd8a40144235bef
touhouota <touhouota.register@gmail.com>
2018-05-27 18:19:25 +0900

    削除。

--- lib/capistrano/tasks/compile.rb
+++
@@ -1,7 +0,0 @@
-namespace :compile do
-  desc 'デプロイ作業'
-  task :webpack do
-    `./node_modules/.bin/webpack --config ./webpack.config.production.js`
-  end
-end
-before 'deploy:assets:precompile', 'compile:webpack'
