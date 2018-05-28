5095204b68849a5b92d2cb159958b8978e2be006
touhouota <touhouota.register@gmail.com>
2018-05-28 23:23:24 +0900

    ページの基本的なJSファイルを追加。

---
+++ client/task_structure.jsx
@@ -0,0 +1,9 @@
+import react from 'react';
+import ReactDOM from 'react-dom';
+
+window.onload = () => {
+  ReactDOM.render(
+    <div>hello TaskStructure</div>
+    document.querySelector('.container')
+  );
+};
