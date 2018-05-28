d20134b7532ba343982d773badbc552a10e7d389
touhouota <touhouota.register@gmail.com>
2018-05-28 23:33:43 +0900

    Reactの綴が違った。

--- app/assets/javascripts/task_structure.js
+++ app/assets/javascripts/task_structure.js
@@ -79,7 +79,7 @@
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-eval("\n\nvar _react = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n\nvar _react2 = _interopRequireDefault(_react);\n\nvar _reactDom = __webpack_require__(/*! react-dom */ \"./node_modules/react-dom/index.js\");\n\nvar _reactDom2 = _interopRequireDefault(_reactDom);\n\nfunction _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }\n\nwindow.onload = function () {\n  _reactDom2.default.render(React.createElement(\n    'div',\n    null,\n    'hello TaskStructure'\n  ), document.querySelector('.container'));\n};\n\n//# sourceURL=webpack:///./client/task_structure.jsx?");
+eval("\n\nvar _react = __webpack_require__(/*! react */ \"./node_modules/react/index.js\");\n\nvar _react2 = _interopRequireDefault(_react);\n\nvar _reactDom = __webpack_require__(/*! react-dom */ \"./node_modules/react-dom/index.js\");\n\nvar _reactDom2 = _interopRequireDefault(_reactDom);\n\nfunction _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }\n\nwindow.onload = function () {\n  _reactDom2.default.render(_react2.default.createElement(\n    'div',\n    null,\n    'hello TaskStructure'\n  ), document.querySelector('.container'));\n};\n\n//# sourceURL=webpack:///./client/task_structure.jsx?");
 
 /***/ }),
 
