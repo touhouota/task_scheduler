b4f185fbed81524eaf9a679fc812378d7e10dfa4
touhouota <touhouota.register@gmail.com>
2018-05-30 22:10:17 +0900

    タスクを実行するためのタイマーを実行する部分を追加。

--- client/task_structure.jsx
+++ client/task_structure.jsx
@@ -4,10 +4,12 @@ import ReactDOM from 'react-dom';
 import Structure from './components/TaskStructure/Structure';
 
 import Buttons from './lib/buttons_events';
+import TimerManager from './lib/time_manager';
 
 window.onload = () => {
   document.getElementById('append_task').addEventListener('click', Buttons.append_task);
   document.getElementById('github').addEventListener('click', Buttons.github);
+  TimerManager.watch();
 
   ReactDOM.render(
     <div className="Structure_Component">
