f1197e23f2990c3ef9c3ae1a70407be6185cd617
touhouota <touhouota.register@gmail.com>
2018-06-02 01:15:30 +0900

    cookies.length

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -41,7 +41,7 @@ class ApiController < ApplicationController
       expect_minute: params[:expect_minute]
     }
     # テスト用
-    render json: [request.headers['Cookies'], params]
+    render json: [cookies.length]
     # user = User.find_by(user_id: cookies[:user_id])
     # @task = user.tasks.build(task_info)
     # if @task.save
