0712d89263debde5e4a62241a4806a145de6bf7c
touhouota <touhouota.register@gmail.com>
2018-06-02 00:48:33 +0900

    送信するだけのボタンを追加

--- client/components/TaskStructure/Structure.jsx
+++ client/components/TaskStructure/Structure.jsx
@@ -81,6 +81,19 @@ class Structure extends React.Component {
   render() {
     return (
       <div className="Structure">
+        <button onClick={() => {
+          const path = Base.get_path();
+          fetch(`${path}/api/tasks/create`, {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+              Accept: 'application/json',
+              'X-CSRF-Token': Base.get_token(),
+            },
+            body: new FormData(),
+          });
+        }}
+        />
         {this.createStructureElements(this.state.tasks)}
         <Modal
           updateTaskList={this.updateTaskList}
