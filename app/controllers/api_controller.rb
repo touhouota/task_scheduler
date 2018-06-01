cdd32c5d6378a45d7e0d320c5b3dd8ea0bfa8ecb
touhouota <touhouota.register@gmail.com>
2018-06-02 00:36:06 +0900

    @requestをつけると良い？

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -41,7 +41,7 @@ class ApiController < ApplicationController
       expect_minute: params[:expect_minute]
     }
     # テスト用
-    render json: [cookies[:user_id], params]
+    render json: [@request.cookies[:user_id], params]
     # user = User.find_by(user_id: cookies[:user_id])
     # @task = user.tasks.build(task_info)
     # if @task.save
