ddf6ebd333442acb49d622b28914a695edb1e2c0
touhouota <touhouota.register@gmail.com>
2018-06-02 01:08:44 +0900

    hoge

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -41,7 +41,7 @@ class ApiController < ApplicationController
       expect_minute: params[:expect_minute]
     }
     # テスト用
-    render json: [request.headers, cookies, params]
+    render json: [request.headers['Cookies'], Cookies, params]
     # user = User.find_by(user_id: cookies[:user_id])
     # @task = user.tasks.build(task_info)
     # if @task.save
