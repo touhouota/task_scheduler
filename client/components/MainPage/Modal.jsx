7774c5938d55b40885510866187dde9d4e5aa9ce
touhouota <touhouota.register@gmail.com>
2018-06-02 10:17:35 +0900

    リクエスト時に、user_idを送る

--- client/components/MainPage/Modal.jsx
+++ client/components/MainPage/Modal.jsx
@@ -104,6 +104,7 @@ class Modal extends React.Component {
     console.log('send form information');
     const form = document.getElementById('modal_area');
     const formData = ModalProcess.getModalData(form);
+    formData.append('user_id', Base.get_cookie('user_id'));
     console.log(formData);
     const path = Base.get_path();
     fetch(`${path}/api/tasks/create`, {
