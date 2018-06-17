d4b426469f656708abbceb61110cec969a485670
touhouota <touhouota.register@gmail.com>
2018-06-17 16:44:07 +0900

    APIにリクエストを投げて、描画するようにした。

--- client/components/TaskStructure/MembersTask.jsx
+++ client/components/TaskStructure/MembersTask.jsx
@@ -7,9 +7,9 @@ class MembersTask extends React.Component {
   constructor(props) {
     super(props);
     console.log('MembersTask');
-    this.setState({
+    this.state = {
       members: [],
-    });
+    };
 
     this.createMembersTaskNumList = this.createMembersTaskNumList.bind(this);
     this.getMembersTaskNumRequest = this.getMembersTaskNumRequest.bind(this);
@@ -19,7 +19,7 @@ class MembersTask extends React.Component {
   getMembersTaskNumRequest() {
     const path = Base.get_path();
     const userId = Base.get_cookie('user_id');
-    fetch(`${path}/api/tasks/membersTask`, {
+    fetch(`${path}/api/tasks/membersTask/${userId}`, {
       credentials: 'same-origin',
       headers: {
         Accept: 'application/json',
@@ -35,29 +35,24 @@ class MembersTask extends React.Component {
   }
 
   createMembersTaskNumList() {
-    return this.props.members.map(member => (
-      <li>
-        {member.name}
+    return this.state.members.map(member => (
+      <li key={member.user_id} className="member">
+        <span className={member.user_id}>{member.u_name}</span>:
+        <span className="task_num">{member.finish_num}/{member.task_num}</span>
       </li>
     ));
   }
 
   render() {
+    console.log(this.state);
     return (
-      <ul>
-        {this.createMembersTaskNumList()}
-      </ul>
+      <div className="members_task">
+        <ul>
+          {this.createMembersTaskNumList()}
+        </ul>
+      </div>
     );
   }
 }
-
-MembersTask.propTypes = {
-  members: PropTypes.arrayOf(PropTypes.shape({
-    user_id: PropTypes.string,
-    name: PropTypes.string,
-    task_num: PropTypes.number,
-    finish_num: PropTypes.number,
-  })).isRequired,
-};
 
 export default MembersTask;
