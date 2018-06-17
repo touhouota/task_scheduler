ad642458cbe4e2996615e9cddeb8ca8e8df3338f
touhouota <touhouota.register@gmail.com>
2018-06-17 21:07:06 +0900

    仲間の進みを表示するためのイベントを追加。

--- client/lib/buttons_events.js
+++ client/lib/buttons_events.js
@@ -2,13 +2,23 @@ import ModalProcess from './modal_process';
 import Base from './base_object';
 
 const Buttons = {
+  append_task: () => {
+    ModalProcess.showModal();
+  },
+
+  members: () => {
+    console.log('members_status');
+    ModalProcess.init();
+    const membersTask = document.querySelector('.members_status');
+    membersTask.classList.remove('hide');
+    ModalProcess.getModalBack().addEventListener('click', () => {
+      membersTask.classList.add('hide');
+    });
+  },
+
   github: () => {
     const URL = 'https://github.com/touhouota/task_scheduler/issues';
     window.open(URL, '_blank');
-  },
-
-  append_task: () => {
-    ModalProcess.showModal();
   },
 
   logout: () => {
