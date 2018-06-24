b2373f9d2892654781e58569098118c7d616b302
touhouota <touhouota.register@gmail.com>
2018-06-24 15:54:29 +0900

    user_idの処理をコメントアウト。

--- client/task_structure.jsx
+++ client/task_structure.jsx
@@ -9,30 +9,30 @@ import TimerManager from './lib/time_manager';
 import Base from './lib/base_object';
 
 window.onload = () => {
-  if (!Base.get_cookie('user_id')) {
-    /*
-     * IDが無いときの処理
-     */
-    console.log('cookieがないよ！/ ログインページへ =>');
-    const path = Base.get_path();
-    window.location.href = `${path}`;
-  } else {
-    /*
-     * IDがあるときは、普通にreactのあれこれを描画
-     */
-    document.getElementById('append_task').addEventListener('click', Buttons.append_task);
-    document.getElementById('member_status').addEventListener('click', Buttons.members);
-    document.getElementById('github').addEventListener('click', Buttons.github);
-    TimerManager.watch();
+  // if (!Base.get_cookie('user_id')) {
+  //   /*
+  //    * IDが無いときの処理
+  //    */
+  //   console.log('cookieがないよ！/ ログインページへ =>');
+  //   const path = Base.get_path();
+  //   window.location.href = `${path}`;
+  // } else {
+  //   /*
+  //    * IDがあるときは、普通にreactのあれこれを描画
+  //    */
+  document.getElementById('append_task').addEventListener('click', Buttons.append_task);
+  document.getElementById('member_status').addEventListener('click', Buttons.members);
+  document.getElementById('github').addEventListener('click', Buttons.github);
+  TimerManager.watch();
 
-    ReactDOM.render(
-      <div className="Structure_Component">
-        <Structure
-          TimerManager={TimerManager}
-        />
-        <MembersTask />
-      </div>,
-      document.querySelector('.structure_container'),
-    );
-  }
+  ReactDOM.render(
+    <div className="Structure_Component">
+      <Structure
+        TimerManager={TimerManager}
+      />
+      <MembersTask />
+    </div>,
+    document.querySelector('.structure_container'),
+  );
+  // }
 };
