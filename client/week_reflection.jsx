f9b5f41239a422a8c5d737e5c3d932b9a690f3c9
touhouota <touhouota.register@gmail.com>
2018-07-02 23:41:39 +0900

    週間振り返りを描画する起点を修正。

--- client/week_reflection.jsx
+++ client/week_reflection.jsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 
 import WeekReflection from './components/WeekReflection';
 
+import TimerManager from './lib/time_manager';
+
 // import Base from './lib/base_object';
 
 window.onload = () => {
   ReactDOM.render(
-    <WeekReflection />,
+    <WeekReflection
+      TimerManager={TimerManager}
+    />,
     document.querySelector('.week_area'),
   );
 };
