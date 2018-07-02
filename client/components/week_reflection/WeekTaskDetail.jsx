7c15661672a3caaed0f4cc0191a41ea8c6fb8386
touhouota <touhouota.register@gmail.com>
2018-07-03 01:07:54 +0900

    詳細用のコンポーネントを追加。

---
+++ client/components/week_reflection/WeekTaskDetail.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import TaskDetails from '../TaskStructure/TaskDetails';
+
+class WeekTaskDetails extends TaskDetails {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        WeekTaskDetails
+      </div>
+    );
+  }
+}
+
+export default WeekTaskDetails;
