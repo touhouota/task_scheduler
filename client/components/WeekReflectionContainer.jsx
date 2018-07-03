eb9ab54c9e40e09843adaa94ca063045c355fd44
touhouota <touhouota.register@gmail.com>
2018-07-03 14:45:27 +0900

    状態もなにもないので、関数の形で記述。

--- client/components/WeekReflectionContainer.jsx
+++ client/components/WeekReflectionContainer.jsx
@@ -2,23 +2,15 @@ import React from 'react';
 
 import WeekReflection from './week_reflection/WeekReflection';
 
-class WeekReflectionContainer extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="container">
-        <header />
-        <div className="WeekReflection">
-          <WeekReflection
-            TimerManager={this.props.TimerManager}
-          />
-        </div>
-      </div>
-    );
-  }
-}
+const WeekReflectionContainer = () => (
+  <div className="container">
+    <header />
+    <div className="WeekReflection">
+      <WeekReflection
+        TimerManager={this.props.TimerManager}
+      />
+    </div>
+  </div>
+);
 
 export default WeekReflectionContainer;
