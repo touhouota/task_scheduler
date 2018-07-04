6f1fed1db60a8ab41d76325f41ea7c759d202335
touhouota <touhouota.register@gmail.com>
2018-07-04 18:31:01 +0900

    次以降の変更点をメモ。

--- client/components/WeekReflectionContainer.jsx
+++ client/components/WeekReflectionContainer.jsx
@@ -1,16 +1,35 @@
 import React from 'react';
 
 import WeekReflection from './week_reflection/WeekReflection';
+import WeekHeader from './week_reflection/WeekHeader';
 
-const WeekReflectionContainer = () => (
-  <div className="container">
-    <header />
-    <div className="WeekReflection">
-      <WeekReflection
-        TimerManager={this.props.TimerManager}
-      />
-    </div>
-  </div>
-);
+class WeekReflectionContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.updateTaskList = this.updateTaskList.bind(this);
+  }
+
+  updateTaskList() {
+    // TODO: stateでタスクリストをもたせ、更新するようにする
+    // これより下のコンポーネントから引っ張ってくればいい
+    // 合わせて、WeekReflectionへpropsで渡してくれればいいよ
+  }
+
+  render() {
+    return (
+      <div>
+        <WeekHeader
+          sum={0}
+        />
+        <div className="WeekReflection">
+          <WeekReflection
+            TimerManager={this.props.TimerManager}
+          />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default WeekReflectionContainer;
