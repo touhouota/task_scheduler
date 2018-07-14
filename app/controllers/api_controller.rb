544ef290374ba11ef43371285f6198dc4dac3aa4
touhouota <touhouota.register@gmail.com>
2018-07-14 22:21:57 +0900

    週間振り返りのためのタスク取得をweek_reflectionコントローラへ。

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,18 +1,3 @@
 class ApiController < ApplicationController
   protect_from_forgery with: :null_session
-
-  # 週を指定してタスクを取得(振りかえり利用)
-  def week_reflection
-    @user = User.find_by(user_id: params[:user_id])
-    # 日付の範囲、今日までにすると今日の00:00までとなり、実質昨日までとなるので1日ずらす
-    to = Date.parse(params[:date]) + 1
-    from = to.ago(1.week)
-    # 1週間分のタスクを返す
-    conditions = {
-      user_id: @user.user_id,
-      updated_at: (from..to),
-      status: [2, 3]
-    }
-    render json: Task.where(conditions)
-  end
 end
