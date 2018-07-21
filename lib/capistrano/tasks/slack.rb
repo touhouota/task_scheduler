79b1cbd39328cf3abb0ce5a7fc78a01d9c1d0579
touhouota <touhouota.register@gmail.com>
2018-07-21 15:49:19 +0900

    タスクをデプロイサーバで実行してもらうよう変更。

--- lib/capistrano/tasks/slack.rb
+++ lib/capistrano/tasks/slack.rb
@@ -1,6 +1,13 @@
 namespace :slack do
   desc 'Slackへの接続をする'
   task :connect do
-    load Rails.root.join('config', 'Slack_bot.rb')
+    # load Rails.root.join('config', 'Slack_bot.rb')
+    on roles(:web) do
+      execute <<-EOC
+      cd #{deploy_to}/current; bundle exec ruby config/Slack_bot.rb}
+      EOC
+    end
   end
 end
+
+after 'puma:restart', 'slack:connect'
