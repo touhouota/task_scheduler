f9550a36f17f6c14cab6be0cd276ab09015855a3
touhouota <touhouota.register@gmail.com>
2018-07-23 00:39:07 +0900

    asd

--- lib/capistrano/tasks/slack.rb
+++ lib/capistrano/tasks/slack.rb
@@ -2,7 +2,7 @@ namespace :slack do
   desc 'Slackへの接続をする'
   task :connect do
     on roles(:web) do
-      execute "cd #{deploy_to}/current; nohup bundle exec rails slack_bot:connect &; echo !$ > #{fetch[:slack_pid]}"
+      execute "cd #{fetch[:deploy_to]}/current; nohup bundle exec rails slack_bot:connect &; echo !$ > #{fetch[:slack_pid]}"
     end
   end
 
