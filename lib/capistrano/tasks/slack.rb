dd781cebed0c8b8a85bb65dcc466604b2abf4979
touhouota <touhouota.register@gmail.com>
2018-07-22 23:17:48 +0900

    閉じるためのタスクを追加。

--- lib/capistrano/tasks/slack.rb
+++ lib/capistrano/tasks/slack.rb
@@ -2,7 +2,14 @@ namespace :slack do
   desc 'Slackへの接続をする'
   task :connect do
     on roles(:web) do
-      execute "cd #{deploy_to}/current; bundle exec rails slack_bot:connect"
+      execute "cd #{deploy_to}/current; nohup bundle exec rails slack_bot:connect &"
+    end
+  end
+
+  desc '接続を切る'
+  task :disconnect do
+    on roles(:web) do
+      execute :kill, "-9 #{`ps x | grep slack_bot:connect`.split("\n").select { |str| str.include?('/usr') }.first.split(' ').first}"
     end
   end
 end
