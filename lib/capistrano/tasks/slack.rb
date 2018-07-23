3b7356b6ffc45bae0d9f99b7506fcee8ad1a3961
touhouota <touhouota.register@gmail.com>
2018-07-23 10:15:42 +0900

    少し変更。

--- lib/capistrano/tasks/slack.rb
+++ lib/capistrano/tasks/slack.rb
@@ -10,16 +10,19 @@ namespace :slack do
   desc 'Slackへの接続をする'
   task :connect do
     on roles(:web) do
-      execute "cd #{fetch :deploy_to}/current; (nohup bundle exec rails slack_bot:connect & echo $! > #{fetch :slack_pid});"
+      execute "cd #{fetch :deploy_to}/current; (nohup bundle exec rails slack_bot:connect < /dev/null > /dev/null & echo $! > #{fetch :slack_pid})"
     end
   end
 
   desc '接続を切る'
   task :disconnect do
     on roles(:web) do
-      pid = capture "cat #{fetch :slack_pid}"
-      p pid
-      execute :kill, "-9 #{pid}"
+      begin
+        pid = capture "cat #{fetch :slack_pid}"
+        execute :kill, "-9 #{pid}"
+      rescue StandardError => e
+        puts "#{e.class} |||| #{e.message}"
+      end
     end
   end
 end
