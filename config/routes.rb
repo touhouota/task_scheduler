6ac0dc8df2ab8cfd000aed16a62395fb8806deea
touhouota <touhouota.register@gmail.com>
2018-07-23 20:20:26 +0900

    コード整理時に、API側のリクエストURLと実際に処理する部分の連携を直し忘れていた。

--- config/routes.rb
+++ config/routes.rb
@@ -30,7 +30,7 @@ Rails.application.routes.draw do
   # API用のルート
   get '/api/tasks', to: 'task_structure#tasks'
   get '/api/tasks/:user_id', to: 'task_structure#user_tasks'
-  post '/api/tasks/create', to: 'api#insert_task'
+  post '/api/tasks/create', to: 'task_structure#insert_task'
   get '/api/tasks/membersTask/:user_id', to: 'task_structure#getMembersTask'
   post '/api/task/statusChange', to: 'task_structure#statusChange'
   post '/api/task/modify', to: 'task_structure#taskModify'
