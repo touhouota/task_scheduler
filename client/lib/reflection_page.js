2f80fa27c48690c511742012a4aeea356d170113
touhouota <touhouota@touhouotanoMacBook-Pro.local>
2018-10-10 21:01:42 +0900

    処理の流れを変更。 ReflectionPageのdrawPageがすべての起点となる。

--- client/lib/reflection_page.js
+++ client/lib/reflection_page.js
@@ -2,22 +2,34 @@ import Graph from './graph';
 import Base from './base_object';
 
 const ReflectionPage = {
-  init: (canvasId) => {
+  // ページを描画する
+  drawPage: (canvasId) => {
+    // グラフの初期化
+    ReflectionPage.__init(canvasId);
+    ReflectionPage.__getGraphData();
+  },
+  // Canvasの初期化
+  __init: (canvasId) => {
     Graph.init(canvasId);
   },
-  drawGraph: (tasks) => {
-    Graph.setValue(tasks);
-    Graph.draw();
-  },
-  getGraphData: () => {
+  __getGraphData: () => {
     const userId = Base.get_cookie('user_id');
     const path = `${Base.get_path()}/api/individual/${userId}`;
     fetch(path)
       .then(response => response.json())
       .then((json) => {
-        console.table(json);
-        ReflectionPage.drawGraph(json.task_info);
+        // グラフ表示
+        ReflectionPage.__drawGraph(json.task_info);
+        // グラフのラベル付け
+        ReflectionPage.__setLabelValue(json.task_info);
       });
+  },
+  __drawGraph: (tasks) => {
+    Graph.setValue(tasks);
+    Graph.draw();
+  },
+  __setLabelValue: (labels) => {
+    console.table(json);
   },
 };
 
