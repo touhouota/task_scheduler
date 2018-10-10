d98a11cfd2f29663af9a4df0ca38b471e31bc9ba
touhouota <touhouota@touhouotanoMacBook-Pro.local>
2018-10-10 12:21:23 +0900

    Canvasの初期化をする関数を定義。

--- client/lib/graph.js
+++ client/lib/graph.js
@@ -1,7 +1,15 @@
 const Graph = {
+  // canvasのdomを保持
+  canvas: null,
+  // canvasの描画関連を司るcontextを保持
+  context: null,
+  init: (canvasId) => {
+    Graph.canvas = document.getElementById(canvasId);
+    Graph.context = Graph.canvas.getContext('2d');
+  },
   hello: () => {
-    console.log("Hello Graph");
-  }
-}
+    console.log('Hello Graph');
+  },
+};
 
 export default Graph;
