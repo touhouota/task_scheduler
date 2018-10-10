b353cadca6c43a2a562c1f55d4e7f801091bf3a9
touhouota <touhouota@touhouotanoMacBook-Pro.local>
2018-10-10 22:04:26 +0900

    それぞれのラベルごとの作業割合を表示するようにした。

--- client/lib/reflection_page.js
+++ client/lib/reflection_page.js
@@ -21,15 +21,28 @@ const ReflectionPage = {
         // グラフ表示
         ReflectionPage.__drawGraph(json.task_info);
         // グラフのラベル付け
-        ReflectionPage.__setLabelValue(json.task_info);
+        ReflectionPage.__calcRate(json.task_info);
       });
   },
   __drawGraph: (tasks) => {
     Graph.setValue(tasks);
     Graph.draw();
   },
-  __setLabelValue: (labels) => {
-    console.table(json);
+  __calcRate: (labels) => {
+    const labelList = ['survay', 'develop', 'experiment', 'write'];
+    let maxNum = 0;
+    Object.values(labels).forEach((item) => {
+      maxNum += item;
+    });
+    labelList.forEach((label) => {
+      const rate = Base.round_at(labels[label] / maxNum, 1) * 100;
+      ReflectionPage.__setLabelValue(label, rate);
+    });
+  },
+  __setLabelValue: (label, rate) => {
+    console.log(label, rate);
+    const target = document.getElementById(label);
+    target.querySelector('.value').textContent = rate.toString();
   },
 };
 
