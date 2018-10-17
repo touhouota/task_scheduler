c252cdfc06ffb2cea11ab970b481153cf492131f
touhouota <touhouota.register@gmail.com>
2018-10-17 16:41:49 +0900

    Userコンポーネントを読み込むよう変更。

--- client/components/members_reflection/reflection_main.jsx
+++ client/components/members_reflection/reflection_main.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
 
+import User from './user';
+import Base from '../../lib/base_object';
+
 class ReflectionMain extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.getMembersInfo = this.getMembersInfo.bind(this);
+
+    this.getMembersInfo();
+  }
+
+  getMembersInfo() {
+    const path = Base.get_path();
+    const userId = Base.get_cookie('user_id');
+    fetch(`${path}/api/members/${userId}`, {
+      credentials: 'same-origin',
+      headers: {
+        Accept: 'application/json',
+        'X-CSRF-Token': Base.get_token(),
+      },
+    })
+      .then(res => res.json())
+      .then((json) => {
+        console.log(json);
+      });
+  }
+
+
   render() {
     return (
       <div>
-        hoge
+        <User />
       </div>
     );
   }
