7da306868faa549680b3c2b0b021ca03b871d2a3
touhouota <touhouota.register@gmail.com>
2018-10-17 18:07:07 +0900

    UserコンポーネントでCanvasを定義し、キャンバス関連の初期化処理を追加。

--- client/components/members_reflection/user.jsx
+++ client/components/members_reflection/user.jsx
@@ -1,13 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import Graph from '../../lib/graph';
 
 class User extends React.Component {
+  constructor(props) {
+    super(props);
+
+    console.log(props);
+  }
+
+  componentDidMount() {
+    console.log('DidMount');
+    Graph.init(this.props.user_id);
+  }
+
   render() {
     return (
       <div>
-        user
+        <p>{this.props.user_id}</p>
+        <canvas id={this.props.user_id} className="graph" />
       </div>
     );
   }
 }
+
+User.propTypes = {
+  user_id: PropTypes.string.isRequired,
+  achieve: PropTypes.shape({
+    0: PropTypes.number,
+    1: PropTypes.number,
+    2: PropTypes.number,
+    3: PropTypes.number,
+    4: PropTypes.number,
+  }).isRequired,
+  actual_secs: PropTypes.number.isRequired,
+  task_info: PropTypes.shape({
+    develop: PropTypes.number,
+    experiment: PropTypes.number,
+    survay: PropTypes.number,
+    write: PropTypes.number,
+  }).isRequired,
+};
 
 export default User;
