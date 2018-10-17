c5e6151225f78a02e4dd58c1af0531885bd27308
touhouota <touhouota.register@gmail.com>
2018-10-17 13:55:39 +0900

    ページのメインとなるコンポーネントを作成。

---
+++ client/components/members_reflection/reflection_main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ReflectionMain extends React.Component {
+  render() {
+    return (
+      <div>
+        hoge
+      </div>
+    );
+  }
+}
+
+export default ReflectionMain;
