7560b8aab96f1104dacab8704f00594240b43918
touhouota <touhouota.register@gmail.com>
2018-11-05 17:35:56 +0900

    グラフを置く部分のコンポーネントを追加。

---
+++ client/components/TaskStructure/MembersGraph.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class MembersGraph extends React.Component {
+  constructor() {
+    super();
+    console.log('MembersGraph');
+  }
+
+  render() {
+    return (
+      <div>
+        MembersGraph
+      </div>
+    );
+  }
+}
+
+export default MembersGraph;
---
+++ client/components/TaskStructure/MyGraph.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class MyGraph extends React.Component {
+  constructor() {
+    super();
+    console.log('MyGraph');
+  }
+
+  render() {
+    return (
+      <div>
+        MyGraph
+      </div>
+    );
+  }
+}
+
+export default MyGraph;
