93d66e0a15aa75814f84b6f1e7e5f58266639c9b
touhouota <touhouota.register@gmail.com>
2018-12-20 14:18:11 +0900

    slackへ投稿する部分の実装。

--- app/controllers/checklist_controller.rb
+++ app/controllers/checklist_controller.rb
@@ -1,3 +1,5 @@
+require 'http'
+
 class ChecklistController < ApplicationController
   # indexメソッドのみ、CSRF対策
   protect_from_forgery only: ['index']
@@ -28,7 +30,10 @@ class ChecklistController < ApplicationController
         status: 'create'
       }
 
+      # Slackへ投稿する
+      post_slack(checklist_permit[:box_name], checklist_permit[:user_id])
     else
+      # 要素が存在する場合、削除する
       result = {
         data: checklist.first.destroy,
         status: 'destroy'
@@ -53,6 +58,45 @@ class ChecklistController < ApplicationController
   end
 
   def checklist_permit
+    puts "permit: #{params.permit(:user_id, :box_name)}"
     params.permit(:user_id, :box_name)
+  end
+
+  def post_slack(text = 'test_test', user_id)
+    target = 'https://slack.com/api/chat.postMessage'
+    body = {
+      token: ENV['SLACK_API_TOKEN'],
+      channel: '#times_nakahara2',
+      text: create_message(text, user_id),
+      as_user: true
+    }
+    HTTP.post(target, params: body)
+  end
+
+  def create_message(box_name = 'hoge_fuga', user_id)
+    user = User.find_by(user_id: user_id)
+    label = box_name.split('_').first
+    report_checklist = {
+      title: 'タイトル(日・英)',
+      abstj: 'アブストラクト(日本語)',
+      abste: 'アブストラクト(英語)',
+      intro: '導入・はじめに',
+      scholar: '関連研究',
+      product: '制作物・システム',
+      exp: '方法・実験',
+      result: '結果',
+      consider: '考察',
+      concl: 'まとめ・結論',
+      thanks: '謝辞',
+      ref: '参考文献',
+      appendix: '付録',
+      define: '言葉の定義',
+      connect: '文の接続',
+      struct: '各章のはじめ'
+    }
+
+    puts user[:u_name], label, report_checklist
+
+    "#{user[:u_name]}さんは「#{report_checklist[label.to_sym]}」の項目にチェックを付けました"
   end
 end
