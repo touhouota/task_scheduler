4f1dc8b6635e5f57a4ee90b726ab6682d49fc456
touhouota <touhouota.register@gmail.com>
2019-01-03 17:21:00 +0900

    全ての項目をcheckした時は、その部分に印をつけるようにした。

--- client/share_checklist_progress.js
+++ client/share_checklist_progress.js
@@ -1,19 +1,33 @@
 import Base from './lib/base_object';
 import Buttons from './lib/buttons_events';
 
+const checkFullChecked = (record) => {
+  const target = Base.parents(record.pop().target, 'column');
+  console.log('target', target);
+  target.classList.add('fulled');
+};
+
+const changeWatcher = new MutationObserver(checkFullChecked);
+
+
 const setCheckNum = () => {
   const userId = Base.get_cookie('user_id');
-  const path = `${Base.get_path()}//api/checklist/check_num/${userId}`;
+  const path = `${Base.get_path()}/api/checklist/check_num/${userId}`;
   fetch(path)
     .then(res => res.json())
     .then((json) => {
       const list = document.querySelector(`.${Base.get_cookie('user_id')}`);
-      // const length = json.length;
       for (let key in json) {
-        list.querySelector(`.${key} > `).textContent = json[key];
+        console.table(json);
+        let target = list.querySelector(`.${key} > .num`);
+        changeWatcher.observe(target, {
+          childList: true,
+          subtree: true,
+        });
+        target.textContent = json[key];
       }
-    })
-}
+    });
+};
 
 window.onload = () => {
   document.getElementById('task_list').addEventListener('click', Buttons.taskList);
